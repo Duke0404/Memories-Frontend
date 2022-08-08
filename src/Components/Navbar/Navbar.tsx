51b0e53de850c55b3b85ea5e702cf14c7a79dc0e
Duke0404 <devansh0404@gmail.com>
2022-08-08 12:54:40 +0200

    Tried to style icons

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 
 //Icons
 import { GrHome, GrCircleInformation, GrFlows } from "react-icons/gr"
+import { IconContext } from "react-icons"
 
 const Navbar = (): JSX.Element => (
     <nav
@@ -11,64 +12,70 @@ const Navbar = (): JSX.Element => (
             justify-center
         "
     >
-        <ul className="flex gap-4">
-            <li
-                className="
-                    inline
-                    px-1.5
-                    pb-1.5
-                    border-b
-                    border-text-1
-                    dark:border-text-1-dark
-                    cursor-pointer
-                    hover:border-accent-secondary-2
-                    hover:border-b-2
-                    active:border-accent-secondary-1
-                "
-            >
-                <Link to="/">
-                    <GrHome className="inline mb-0.5 text-text-1 dark:text-text-1-dark" /> Home
-                </Link>
-            </li>
+        <IconContext.Provider
+            value={{
+                color: "white",
+            }}
+        >
+            <ul className="flex gap-4">
+                <li
+                    className="
+                        inline
+                        px-1.5
+                        pb-1.5
+                        border-b
+                        border-text-1
+                        dark:border-text-1-dark
+                        cursor-pointer
+                        hover:border-accent-secondary-2
+                        hover:border-b-2
+                        active:border-accent-secondary-1
+                    "
+                >
+                    <Link to="/">
+                        <GrHome className="inline mb-0.5 text-text-1 dark:text-text-1-dark" /> Home
+                    </Link>
+                </li>
 
-            <li
-                className="
-                    inline
-                    px-1.5
-                    pb-1.5
-                    border-b
-                    border-text-1
-                    dark:border-text-1-dark
-                    cursor-pointer
-                    hover:border-accent-secondary-2
-                    hover:border-b-2
-                    active:border-accent-secondary-1
-                "
-            >
-                <Link to="/about">
-                    <GrCircleInformation className="inline mb-0.5" /> About
-                </Link>
-            </li>
+                <li
+                    className="
+                        inline
+                        px-1.5
+                        pb-1.5
+                        border-b
+                        border-text-1
+                        dark:border-text-1-dark
+                        cursor-pointer
+                        hover:border-accent-secondary-2
+                        hover:border-b-2
+                        active:border-accent-secondary-1
+                    "
+                >
+                    <Link to="/about">
+                        <GrCircleInformation className="inline mb-0.5" /> About
+                    </Link>
+                </li>
 
-            <li
-                className="
-                    inline
-                    px-1.5
-                    pb-1.5
-                    border-b
-                    border-text-1
-                    dark:border-text-1-dark
-                    cursor-pointer
-                    hover:border-accent-secondary-2
-                    hover:border-b-2
-                    active:border-accent-secondary-1
-                "
-            >
-                <Link to="/all">
-                    <GrFlows className="inline mb-0.5" /> All Entries
-                </Link>
-            </li>
-        </ul>
+                <li
+                    className="
+                        inline
+                        px-1.5
+                        pb-1.5
+                        border-b
+                        border-text-1
+                        dark:border-text-1-dark
+                        cursor-pointer
+                        hover:border-accent-secondary-2
+                        hover:border-b-2
+                        active:border-accent-secondary-1
+                    "
+                >
+                    <Link to="/all">
+                        <GrFlows className="inline mb-0.5" /> All Entries
+                    </Link>
+                </li>
+            </ul>
+        </IconContext.Provider>
     </nav>
 )
 
